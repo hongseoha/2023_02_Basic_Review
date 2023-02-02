10823f624b5a59e8282152adca634be65fc8c308
hongseoha <seoseo.ha.h@gmail.com>
2023-02-02 19:53:08 +0900

    스캐너로 명령어입력하기

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[]args) {
+		System.out.println("==Start Program==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("명령어: ");
+		String cmd = sc.nextLine();
+		
+		System.out.printf("입력된 명령어 : %s\n", cmd);
+		
+		System.out.println("==End Program==");
+	}
+}
